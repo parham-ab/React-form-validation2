f2c72cf7ed185c93e15c86efc42a33a595b5558c
Parham Abolghasemi <parhamab17@gmail.com>
2022-05-01 09:42:27 +0430

    login component functionality done

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
+// react toastify
+import { ToastContainer } from "react-toastify";
+import { notify } from "../functions/toast";
 // components
 import ValidationError from "./ValidationError";
 // img
@@ -20,13 +24,22 @@ const validationSchema = Yup.object({
 });
 
 const LogIn = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  const [userDataLogin, setUserDataLogin] = useState([]);
+  const [vals, setVals] = useState([]);
+  const postHandle = () => {
+    axios
+      .post(`https://jsonplaceholder.typicode.com/posts`, {
+        userData: vals,
+      })
+      .then((response) => {
+        setUserDataLogin(response.data);
+        notify("success", "Logged In successfully !");
+      })
+      .catch((error) => notify("error", "Something went wrong !"));
+  };
 
   return (
-    <div className="signUp-container">
+    <div className="login-container">
       <div className="svg-icon">
         <img width={"600px"} src={pic} alt="pic" />
       </div>
@@ -35,50 +48,68 @@ const LogIn = () => {
         <Formik
           initialValues={SignInInitialValues}
           validationSchema={validationSchema}
-          onSubmit={(values) => setData(values)}
+          onSubmit={(values) => setUserDataLogin(values)}
         >
-          <Form>
-            <div>
-              <h1 className="title">Login</h1>
-            </div>
+          {(Formik) => {
+            setVals(Formik.values);
+            return (
+              <Form>
+                <div>
+                  <h1 className="title">Login</h1>
+                </div>
 
-            <div className="field-container">
-              <div>
-                <label htmlFor="email">Email</label>
-              </div>
-              <Field
-                className="customField"
-                type="email"
-                name="email"
-                id="email"
-              />
-              <ErrorMessage name="email" component={ValidationError} />
-            </div>
+                <div className="field-container">
+                  <div>
+                    <label htmlFor="email">Email</label>
+                  </div>
+                  <Field
+                    className="customField"
+                    type="email"
+                    name="email"
+                    id="email"
+                  />
+                  <ErrorMessage name="email" component={ValidationError} />
+                </div>
 
-            <div className="field-container">
-              <div>
-                <label htmlFor="password">Password</label>
-              </div>
-              <Field
-                className="customField"
-                type="password"
-                name="password"
-                id="password"
-              />
-              <ErrorMessage name="password" component={ValidationError} />
-            </div>
+                <div className="field-container">
+                  <div>
+                    <label htmlFor="password">Password</label>
+                  </div>
+                  <Field
+                    className="customField"
+                    type="password"
+                    name="password"
+                    id="password"
+                  />
+                  <ErrorMessage name="password" component={ValidationError} />
+                </div>
 
-            <button className="submitBtn" type="submit">
-              Login
-            </button>
-            <div className="switch">
-              <p>
-                Dont have an account ? <Link to="/">SignUp</Link>
-              </p>
-            </div>
-          </Form>
+                <button
+                  className="submitBtn"
+                  disabled={
+                    (!Formik.isValid && Object.keys(Formik.errors).length) ||
+                    Formik.values.name === "" ||
+                    Formik.values.email === "" ||
+                    Formik.values.password === "" ||
+                    Formik.values.confirmPassword === ""
+                  }
+                  onClick={postHandle}
+                  type="submit"
+                >
+                  Login
+                </button>
+                <div className="switch">
+                  <p>
+                    Dont have an account ? <Link to="/">SignUp</Link>
+                  </p>
+                </div>
+              </Form>
+            );
+          }}
         </Formik>
       </div>
+        {/* react toastify component */}
+        <ToastContainer />
     </div>
   );
 };
