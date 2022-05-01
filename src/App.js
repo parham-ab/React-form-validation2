43bc0bb6c4c29f852f88e8bcb524ddad79ad56ba
Parham Abolghasemi <parhamab17@gmail.com>
2022-05-01 10:22:21 +0430

    dark mode added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-
+// icons
+import { MdDarkMode, MdLightMode } from "react-icons/md";
 // components
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Wave from "./components/Wave";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div>
+    <div className={darkMode && "dark"}>
+      <div>
+        <span onClick={() => setDarkMode(!darkMode)}>
+          {!darkMode ? (
+            <MdDarkMode className="darkMode" />
+          ) : (
+            <MdLightMode className="lightMode" />
+          )}
+        </span>
+      </div>
       <Routes>
         <Route path="/" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
